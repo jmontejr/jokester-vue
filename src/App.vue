58d29ae0281535bfac5807009e947d58c995774c
José Monte <josecmontejr@gmail.com>
2020-06-23 17:30:24 -0300

    master | create Joke component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-    <div id="app" class="app">
-        <h1 class="app__title">{{ title }}</h1>
-        <h2 class="app__subtitle">Got Jokes?</h2>
-        <button class="btn btn-primary" @click="INIT_JOKES">Add ten random jokes</button>
-        <div>
-            {{$store.state}}
+    <div id="app" class="app container">
+
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="app__title">{{ title }}</h1>
+                <h2 class="app__subtitle">Got Jokes?</h2>
+                <button class="btn btn-primary mt-3" @click="INIT_JOKES">Add ten random jokes</button>
+            </div>
         </div>
+
+        <div class="row mt-5">
+            <joke 
+                :joke="joke" 
+                v-for="(joke, index) in $store.state.jokes" 
+                :key="index" 
+            />
+        </div>
+
     </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
+import Joke from '@/components/Joke.vue';
 
 export default {
     data() {
@@ -19,6 +31,9 @@ export default {
         };
     },
     methods: mapActions([ 'INIT_JOKES' ]),
+    components: {
+        Joke
+    }
 };
 </script>
 
---
+++ src/components/Joke.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="joke col-md-6" id="joke">
+        <div class="joke__card">
+            <div class="joke__card--block">
+                <p><u>{{ joke.setup }}</u></p>
+                <p><em>{{ joke.punchline }}</em></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        joke: {
+            required: true
+        }
+    }
+}
+</script>
